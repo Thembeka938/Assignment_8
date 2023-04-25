5e5159e725a49fd5937732fb4ac3acb259fd3dcd
Thembeka Nguza <Thembeka.Nguza@ekurhuleni.gov.za>
2023-04-25 15:21:54 +0200

    third commit

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -7,43 +8,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
- name:string="";
- surname:string="";
- email:string="";
- town:string="";
- suburb:string="";
- street:string="";
- code:string="";
- password:string="";
- confirmPassword="";
- phone:string="";
- securityQuestion:string="";
- answer:string="";
- constructor(private router:Router){}
+user:any={}
+ 
+ constructor(private router:Router, private userService:UserService){}
  ngOnInit(): void {
+  console.log("user object", this.user)
+ this.user= this.userService.user
  
  }
  recordDetails(){
-  const user={
-    name:this.name,
-    surname:this.surname,
-    email:this.email,
-    address:{
-      town:this.town,
-      suburb:this.suburb,
-      street:this.street,
-      code:this.code
-    },
-    password:this.password
-  };
+  
  const usersA=JSON.parse(localStorage.getItem('users')||'[]');
-  const foundUser =usersA.find((user:any)=>user.email==this.email);
+  const foundUser =usersA.find((user:any)=>user.email=="");
   if(foundUser){
     alert("user exists");
     return;
   }
-  usersA.push(user);
+  usersA.push(this.user);
   localStorage.setItem('clients',JSON.stringify(usersA));
   alert("registered");
   this.router.navigate(['/login'])
---
+++ src/app/user.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  user = {
+    name: "",
+    surname: "",
+    email: "",
+  password: "",
+    confirmPassword: "",
+    phone: "",
+    securityQuestion: "",
+    answer: "",
+  }
+  constructor() { }
+}
